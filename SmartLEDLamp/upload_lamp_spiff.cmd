16d32d9b359f1848780e14ca2964b9f08d0a907b
danielchiaradia <da.chiaradia@googlemail.com>
2020-10-03 15:21:31 +0200

    * Color picker

--- SmartLEDLamp/upload_lamp_spiff.cmd
+++ SmartLEDLamp/upload_lamp_spiff.cmd
@@ -1,16 +1,21 @@
 robocopy data data_bin /E
 
-"C:\Program Files\7-Zip\7z.exe" a -tgzip data_bin\resources\bootstrap.min.css.gzip data_bin\resources\bootstrap.min.css
-"C:\Program Files\7-Zip\7z.exe" a -tgzip data_bin\resources\bootstrap.min.js.gzip data_bin\resources\bootstrap.min.js
-"C:\Program Files\7-Zip\7z.exe" a -tgzip data_bin\resources\jquery-3.1.1.min.js.gzip data_bin\resources\jquery-3.1.1.min.js
+"C:\Program Files\7-Zip\7z.exe" a -tgzip data_bin\resources\bs.min.css.gz data_bin\resources\bs.min.css
+"C:\Program Files\7-Zip\7z.exe" a -tgzip data_bin\resources\bs.min.js.gz data_bin\resources\bs.min.js
+"C:\Program Files\7-Zip\7z.exe" a -tgzip data_bin\resources\jq-3.1.1.min.js.gz data_bin\resources\jq-3.1.1.min.js
+"C:\Program Files\7-Zip\7z.exe" a -tgzip data_bin\resources\pickr.min.js.gz data_bin\resources\pickr.min.js
+"C:\Program Files\7-Zip\7z.exe" a -tgzip data_bin\resources\nano.min.css.gz data_bin\resources\nano.min.css
 
-DEL data_bin\resources\bootstrap.min.css
-DEL data_bin\resources\bootstrap.min.js
-DEL data_bin\resources\jquery-3.1.1.min.js
+DEL data_bin\resources\bs.min.css
+DEL data_bin\resources\bs.min.js
+DEL data_bin\resources\jq-3.1.1.min.js
+DEL data_bin\resources\pickr.min.js
+DEL data_bin\resources\nano.min.css
+
 
 C:\Users\dchiarad\.arduinocdt\packages\esp8266\tools\mkspiffs\0.2.0\mkspiffs.exe -p 256 -b 8192 -s 3125248 -c data_bin spiffs-image.bin
 
 python C:\Users\dchiarad\.arduinocdt\packages\esp8266\hardware\esp8266\2.3.0\tools\espota.py -i 192.168.178.22 -s -f "spiffs-image.bin"
 
-rmdir /S /Q data_bin
+REM rmdir /S /Q data_bin
 pause
